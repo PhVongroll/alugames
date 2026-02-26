fcb0e7352fdeddbfe7cf3003d7dad18b88695d2f
PhVongroll <phvongroll1208@gmail.com>
2026-02-26 15:31:33 -0300

    Confirmação de devolução

--- js/app.js
+++ js/app.js
@@ -2,15 +2,17 @@ function alterarStatus(id) {
     let gameSelecionado = document.getElementById(`game-${id}`);
     let imagem = gameSelecionado.querySelector('.dashboard__item__img');
     let botao = gameSelecionado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameSelecionado.querySelector('.dashboard__item__name');
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
-
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
     }
-}       
+}
