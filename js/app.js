2040dafd813f398cd8d90b2903487c7d8b7056e3
PhVongroll <phvongroll1208@gmail.com>
2026-02-26 15:24:19 -0300

    Projeto Alugames

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function alterarStatus(id) {
+    let gameSelecionado = document.getElementById(`game-${id}`);
+    let imagem = gameSelecionado.querySelector('.dashboard__item__img');
+    let botao = gameSelecionado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+}       
